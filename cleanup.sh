7bc401a496f7b761a5aa75ccb1ba4a9026b1b75c
Savaonu <60708883+Savaonu@users.noreply.github.com>
2022-03-22 14:43:25 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-container=$(docker ps -a --filter "name=my_app" --format '{{.Names}}')
+container=$(docker ps -a --filter "name=my_maven_app" --format '{{.Names}}')
 
 # Stop and delete containers
 if [ -z "$container" ] ; then echo "No containers to delete"; else echo "The following container(s) will be deleted: "; docker stop $container && docker rm $container ; fi
