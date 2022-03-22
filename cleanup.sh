4b965c5a6cbb01e5fa6080de6e7c48c14d6ac7b2
Savaonu <60708883+Savaonu@users.noreply.github.com>
2022-03-22 11:42:59 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -3,4 +3,4 @@
 container=$(docker ps -a --filter "name=my_app" --format '{{.Names}}')
 
 # Stop and delete containers
-if [ $((docker ps -a --filter "name=my_app") | wc -l) -eq 0 ] ; then echo "No containers to delete"; else echo "The following container(s) will be deleted: "; docker stop $container && docker rm $container ; fi
+if [ -z "$container" ] ; then echo "No containers to delete"; else echo "The following container(s) will be deleted: "; docker stop $container && docker rm $container ; fi
