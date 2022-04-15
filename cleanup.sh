56027a3dfec177e43c288929a1a8e5cfe3b4f54d
sabinstoica <99193344+sabinstoica@users.noreply.github.com>
2022-04-15 12:38:01 +0300

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-container=$(docker ps -a --filter "name=my_maven_app" --format '{{.Names}}')
-
-# Stop and delete containers
-if [ -z "$container" ] ; then echo "No containers to delete"; else echo "The following container(s) will be deleted: "; docker stop $container && docker rm $container ; fi
-
-# Delete old images
-images=$(docker image ls --filter label=type=maven_image  --format="{{.ID}}")
-if [ -z "$images" ]; then echo "No images to delete"; else echo "The following image(s) will be deleted: "; docker rmi $(docker image ls --filter label=type=maven_image  --format="{{.ID}}"); fi
+container=maven_container
+for rcontainers in $(docker ps | grep $container | awk '{print $1}'); do docker stop $rcontainers; done
+for containers in $(docker ps -a | grep $container | awk '{print $1}'); do docker rm $containers; done
+for images in $(docker images | grep $image | awk '{print $3}'); do docker rmi $images; done
